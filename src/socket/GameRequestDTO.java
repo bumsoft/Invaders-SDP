8a31096496a64e0e53a2fbf50e42c70cbf201340
신진범 <shjb07015@gmail.com>
2024-10-31 15:37:47 +0900

    웹 소켓 라이브러리 추가 / 클라이언트 예시코드

---
+++ src/socket/GameRequestDTO.java
@@ -0,0 +1,19 @@
+package socket;
+
+public class GameRequestDTO {
+    private String order;
+
+    public GameRequestDTO(String order) {
+        this.order = order;
+    }
+
+    public String getOrder()
+    {
+        return order;
+    }
+
+    public void setOrder(String order)
+    {
+        this.order = order;
+    }
+}
