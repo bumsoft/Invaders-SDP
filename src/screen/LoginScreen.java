09da4f592f6a2cbd5b3bdfabc335351a8062fb84
dohee <kkdh6322@naver.com>
2024-11-16 15:14:07 +0900

    LoginScreen 및 RegisterScreen method 생성

---
+++ src/screen/LoginScreen.java
@@ -0,0 +1,8 @@
+package screen;
+
+public class LoginScreen extends Screen {
+    public LoginScreen(int width, int height, int fps) {
+        super(width, height, fps);
+    }
+
+}
---
+++ src/screen/RegisterScreen.java
@@ -0,0 +1,9 @@
+package screen;
+
+public class RegisterScreen extends Screen {
+
+    public RegisterScreen(int width, int height, int fps) {
+        super(width, height, fps);
+    }
+
+}
