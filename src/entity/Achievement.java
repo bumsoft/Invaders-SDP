870df99a2df2c93700f2a2b526e0fc187bdc55a1
KIMSEUNGGO <rhtmdrh@hanyang.ac.kr>
2024-11-27 18:46:05 +0900

    Remove unused "playTimeAch" private field and Remove unused mathod "setTotalPlayTime", "setTotalScore", "setHighMaxcombo", "setCurrentPerfectStage", "setFlawlessFailure"

--- src/entity/Achievement.java
+++ src/entity/Achievement.java
@@ -7,7 +7,7 @@ public class Achievement {
     private int maxCombo;
     private int currentPerfectStage;
     private boolean flawlessFailure;
-    private boolean playTimeAch;
+
 
     public Achievement(int totalPlayTime, int totalScore, int maxCombo, int currentPerfectStage,
                        boolean flawlessFailure) {
@@ -24,26 +24,5 @@ public class Achievement {
     public int getHighmaxCombo() { return maxCombo; }
     public int getPerfectStage() { return currentPerfectStage; }
     public boolean getFlawlessFailure() { return flawlessFailure; }
-
-    // Functions to store the status of each achievement.
-    public void setTotalPlayTime(int totalPlayTime) {
-        this.totalPlayTime += totalPlayTime;
-    }
-
-    public void setTotalScore(int totalScore) {
-        this.totalScore += totalScore;
-    }
-
-    public void setHighMaxcombo(int maxCombo) {
-        this.maxCombo = maxCombo;
-    }
-
-    public void setCurrentPerfectStage(int currentPerfectStage) {
-        this.currentPerfectStage = currentPerfectStage;
-    }
-
-    public void setFlawlessFailure(boolean flawlessFailure) {
-        this.flawlessFailure = flawlessFailure;
-    }
 
 }
